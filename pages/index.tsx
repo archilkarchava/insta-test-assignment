c0e70298c17b0010fab65622257609a1ba147fe4
Archil Karchava <archil1997@gmail.com>
2020-11-04 17:05:47 +0500

    Add demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,10 @@ import NumericInput from "components/NumericInput";
 import React, { useState } from "react";
 
 const IndexPage = () => {
+  const [digits, setDigits] = useState<number>(3);
+  const [min, setMin] = useState<number>(-8);
+  const [max, setMax] = useState<number>(40);
+  const [step, setStep] = useState<number>(1);
   const [value, setValue] = useState(0);
   return (
     <>
@@ -12,12 +16,45 @@ const IndexPage = () => {
           <NumericInput
             initialValue={value}
             onChange={(val) => setValue(Number(val))}
-            digits={1}
-            min={-10}
-            max={70}
-            step={0.5}
+            digits={digits}
+            min={min}
+            max={max}
+            step={step}
           />
         </InnerContainer>
+        <TweaksContainer>
+          <div>
+            <h1>Digits</h1>
+            <NumericInput
+              min={0}
+              initialValue={digits}
+              onChange={(val) => setDigits(Number(val))}
+            />
+          </div>
+          <div>
+            <h1>Min</h1>
+            <NumericInput
+              initialValue={min}
+              onChange={(val) => setMin(Number(val))}
+            />
+          </div>
+          <div>
+            <h1>Max</h1>
+            <NumericInput
+              min={0}
+              initialValue={max}
+              onChange={(val) => setMax(Number(val))}
+            />
+          </div>
+          <div>
+            <h1>Step</h1>
+            <NumericInput
+              min={0.1}
+              initialValue={step}
+              onChange={(val) => setStep(Number(val))}
+            />
+          </div>
+        </TweaksContainer>
       </Container>
       <Global
         styles={css`
@@ -29,22 +66,42 @@ const IndexPage = () => {
             padding: 0;
             margin: 0;
           }
+          h1 {
+            margin: 0;
+            padding: 0;
+            font-size: 2rem;
+          }
         `}
       />
     </>
   );
 };
 
+const TweaksContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 36px;
+  margin-left: 14px;
+  margin-right: 14px;
+  div {
+    margin-left: 5px;
+    margin-right: 5px;
+    text-align: center;
+  }
+`;
+
 const Container = styled.div`
   display: flex;
   height: 100%;
   width: 100%;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 `;
 
 const InnerContainer = styled.div`
   width: 50%;
+  /* height: 100%; */
 `;
 
 export default IndexPage;
